1ecb86515e87c0d4d858d6acb7e7929c1939b12c
Clarke Fry <clarkefry@gmail.com>
2018-01-07 16:31:43 -0600

    initial functional files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('All American Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Hamburger', false);
