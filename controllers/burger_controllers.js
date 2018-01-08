cdeed4adcbc637f75eb9413dc92c664e4896bde4
Clarke Fry <clarkefry@gmail.com>
2018-01-08 15:48:55 -0600

    updating routes

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -14,7 +14,7 @@ router.get("/", function(req, res) {
 	});
 });
 
-router.post("/api/burgers", function(req,res) {
+router.post("/", function(req,res) {
 	burger.create([
 		"burger_name", "devoured"
 	],[
