c6b22ec90dbf07b3c7df318890636a7b6f744183
Clarke Fry <clarkefry@gmail.com>
2018-01-08 16:02:31 -0600

    updating config files

--- config/connection.js
+++ config/connection.js
@@ -27,11 +27,11 @@ if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
     connection = mysql.createConnection({
-        root: 3000,
+        // root: 3000,
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'burgers_db',
+        database: 'burgers_db'
     });
 };
 
