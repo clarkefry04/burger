5c62d323cae4306ce779a02ae7c929b5fdebdce3
Clarke Fry <clarkefry@gmail.com>
2018-01-08 11:53:37 -0600

    updated connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,46 @@
+// var mysql = require('mysql');
+// var connection;
+
+// var connection = mysql.createConnection({
+// 	root: 3000,
+// 	host: "localhost",
+// 	user: "root",
+// 	password: '',
+// 	database: "burgers_db"
+// });
+
+// connection.connect(function(err){
+// 	if(err){
+// 		console.log("Error Connecting: " + err.stack);
+// 		return;
+// 	}
+// 	console.log("Connected as id: " + connection.threadId)
+// });
+
+// module.exports = connection;
+
+
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-	port: 3306,
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "burgers_db"
-});
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    connection = mysql.createConnection({
+        root: 3000,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burgers_db',
+    });
+};
 
-connection.connect(function(err){
-	if(err){
-		console.log("Error Connecting: " + err.stack);
-		return;
-	}
-	console.log("Connected as id: " + connection.threadId)
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
