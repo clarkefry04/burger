24bbc085d746ece86173c3bdf2e0fe16931cd377
Clarke Fry <clarkefry@gmail.com>
2018-01-08 16:14:24 -0600

    updating server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var methodOverride = require('method-override')
 var app = express();
 
 //Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + '/public'));
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({
 	extended: false
@@ -16,8 +16,7 @@ app.use(bodyParser.urlencoded({
 app.use(methodOverride('_method'))
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({
-    defaultLayout: 'main',
-}));
+    defaultLayout: "main",}));
 app.set('view engine', 'handlebars');
 
 var routes = require('./controllers/burger_controllers.js');
