873cbdbb19512bf5ad33f5e27af60c5dbd903fc1
Clarke Fry <clarkefry@gmail.com>
2018-01-08 17:04:31 -0600

    updated package.json file

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burger_controllers");
+var routes = require("./controllers/burger_controllers.js");
 
 app.use("/", routes);
 
