a094d6615148b56efa19363494c7c19ab8b1b79e
Clarke Fry <clarkefry@gmail.com>
2018-01-08 16:20:13 -0600

    updating port

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
 
+var express = require("express");
+var bodyParser = require("body-parser");
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override')
+var port = process.env.PORT;
 
 var app = express();
 
-//Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({
-	extended: false
-}))
-// override with POST having ?_method=DELETE
-app.use(methodOverride('_method'))
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-    defaultLayout: "main",}));
-app.set('view engine', 'handlebars');
+var exphbs = require("express-handlebars");
 
-var routes = require('./controllers/burger_controllers.js');
-app.use('/', routes);
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
-var port = process.env.PORT || 3306;
+var routes = require("./controllers/burger_controllers");
+
+app.use("/", routes);
+
 app.listen(port);
